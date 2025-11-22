8592d8ca7664c3a4c66d288e89633619b3870c2f
pachuau10 <duhteapachuau10@gmail.com>
2025-11-22 23:16:29 +0530

    good

--- blog_pach/settings.py
+++ blog_pach/settings.py
@@ -20,8 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ========================
 SECRET_KEY = config('SECRET_KEY', default='your-default-secret-key')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
-
+ALLOWED_HOSTS = [
+    'pachuaublog.up.railway.app',
+    '.railway.app',
+    'localhost',
+    '127.0.0.1',
+    'www.chhohreivung.site',  
+    'chhohreivung.site',      
 # ========================
 # INSTALLED APPS
 # ========================
